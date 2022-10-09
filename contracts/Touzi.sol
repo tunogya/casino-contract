2a0f8ad22f679bbc4ee2c1e4bf3a31ac06dab702
tunogya <tunogya@qq.com>
2022-10-10 00:07:13 +0800

    update

--- contracts/Touzi.sol
+++ contracts/Touzi.sol
@@ -1,39 +1,61 @@
 //SPDX-License-Identifier: MIT
 pragma solidity 0.8.9;
-
 import "@api3/airnode-protocol/contracts/rrp/requesters/RrpRequesterV0.sol";
+import "./interfaces/ITouzi.sol";
 
-contract RemixQrngExample is RrpRequesterV0 {
+contract Touzi is RrpRequesterV0, ITouzi {
     event RequestedUint256(bytes32 indexed requestId);
     event ReceivedUint256(bytes32 indexed requestId, uint256 response);
+    event RequestedUint256Array(bytes32 indexed requestId, uint256 size);
+    event ReceivedUint256Array(bytes32 indexed requestId, uint256[] response);
 
+    // These variables can also be declared as `constant`/`immutable`.
+    // However, this would mean that they would not be updatable.
+    // Since it is impossible to ensure that a particular Airnode will be
+    // indefinitely available, you are recommended to always implement a way
+    // to update these parameters.
     address public airnode;
     bytes32 public endpointIdUint256;
+    bytes32 public endpointIdUint256Array;
     address public sponsorWallet;
-    mapping(bytes32 => bool) public waitingFulfillment;
 
-    // These are for Remix demonstration purposes, their use is not practical.
-    struct LatestRequest {
-        bytes32 requestId;
-        uint256 randomNumber;
-    }
+    mapping(bytes32 => bool) public expectingRequestWithIdToBeFulfilled;
 
-    LatestRequest public latestRequest;
-
+    /// @dev RrpRequester sponsors itself, meaning that it can make requests
+    /// that will be fulfilled by its sponsor wallet. See the Airnode protocol
+    /// docs about sponsorship for more information.
+    /// @param _airnodeRrp Airnode RRP contract address, view https://docs.api3.org/qrng/reference/chains.html
     constructor(address _airnodeRrp) RrpRequesterV0(_airnodeRrp) {}
 
-    // Normally, this function should be protected, as in:
-    // require(msg.sender == owner, "Sender not owner");
+    /// @notice Sets parameters used in requesting QRNG services
+    /// @dev No access control is implemented here for convenience. This is not
+    /// secure because it allows the contract to be pointed to an arbitrary
+    /// Airnode. Normally, this function should only be callable by the "owner"
+    /// or not exist in the first place.
+    /// @param _airnode Airnode address
+    /// @param _endpointIdUint256 Endpoint ID used to request a `uint256`
+    /// @param _endpointIdUint256Array Endpoint ID used to request a `uint256[]`
+    /// @param _sponsorWallet Sponsor wallet address
     function setRequestParameters(
         address _airnode,
         bytes32 _endpointIdUint256,
+        bytes32 _endpointIdUint256Array,
         address _sponsorWallet
     ) external {
+        // Normally, this function should be protected, as in:
+        // require(msg.sender == owner, "Sender not owner");
         airnode = _airnode;
         endpointIdUint256 = _endpointIdUint256;
+        endpointIdUint256Array = _endpointIdUint256Array;
         sponsorWallet = _sponsorWallet;
     }
 
+    /// @notice Requests a `uint256`
+    /// @dev This request will be fulfilled by the contract's sponsor wallet,
+    /// which means spamming it may drain the sponsor wallet. Implement
+    /// necessary requirements to prevent this, e.g., you can require the user
+    /// to pitch in by sending some ETH to the sponsor wallet, you can have
+    /// the user use their own sponsor wallet, you can rate-limit users.
     function makeRequestUint256() external {
         bytes32 requestId = airnodeRrp.makeFullRequest(
             airnode,
@@ -44,24 +66,64 @@ contract RemixQrngExample is RrpRequesterV0 {
             this.fulfillUint256.selector,
             ""
         );
-        waitingFulfillment[requestId] = true;
-        latestRequest.requestId = requestId;
-        latestRequest.randomNumber = 0;
+        expectingRequestWithIdToBeFulfilled[requestId] = true;
         emit RequestedUint256(requestId);
     }
 
+    /// @notice Called by the Airnode through the AirnodeRrp contract to
+    /// fulfill the request
+    /// @dev Note the `onlyAirnodeRrp` modifier. You should only accept RRP
+    /// fulfillments from this protocol contract. Also note that only
+    /// fulfillments for the requests made by this contract are accepted, and
+    /// a request cannot be responded to multiple times.
+    /// @param requestId Request ID
+    /// @param data ABI-encoded response
     function fulfillUint256(bytes32 requestId, bytes calldata data)
     external
     onlyAirnodeRrp
     {
         require(
-            waitingFulfillment[requestId],
+            expectingRequestWithIdToBeFulfilled[requestId],
             "Request ID not known"
         );
-        waitingFulfillment[requestId] = false;
+        expectingRequestWithIdToBeFulfilled[requestId] = false;
         uint256 qrngUint256 = abi.decode(data, (uint256));
         // Do what you want with `qrngUint256` here...
-        latestRequest.randomNumber = qrngUint256;
         emit ReceivedUint256(requestId, qrngUint256);
+    }
+
+    /// @notice Requests a `uint256[]`
+    /// @param size Size of the requested array
+    function makeRequestUint256Array(uint256 size) external {
+        bytes32 requestId = airnodeRrp.makeFullRequest(
+            airnode,
+            endpointIdUint256Array,
+            address(this),
+            sponsorWallet,
+            address(this),
+            this.fulfillUint256Array.selector,
+        // Using Airnode ABI to encode the parameters
+            abi.encode(bytes32("1u"), bytes32("size"), size)
+        );
+        expectingRequestWithIdToBeFulfilled[requestId] = true;
+        emit RequestedUint256Array(requestId, size);
+    }
+
+    /// @notice Called by the Airnode through the AirnodeRrp contract to
+    /// fulfill the request
+    /// @param requestId Request ID
+    /// @param data ABI-encoded response
+    function fulfillUint256Array(bytes32 requestId, bytes calldata data)
+    external
+    onlyAirnodeRrp
+    {
+        require(
+            expectingRequestWithIdToBeFulfilled[requestId],
+            "Request ID not known"
+        );
+        expectingRequestWithIdToBeFulfilled[requestId] = false;
+        uint256[] memory qrngUint256Array = abi.decode(data, (uint256[]));
+        // Do what you want with `qrngUint256Array` here...
+        emit ReceivedUint256Array(requestId, qrngUint256Array);
     }
 }
--- contracts/interfaces/ITouzi.sol
+++ contracts/interfaces/ITouzi.sol
@@ -1,3 +1,60 @@
-interface ITouzi {
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.9;
 
+interface ITouzi {
+    // -------------------------------------------------------------------
+    // Platform functions
+    // -------------------------------------------------------------------
+
+    struct PlatformConfig {
+        address payment;            // Address to receive payments
+        address admin;              // Admin address of the platform
+        uint256 feeRate;            // 0.1% = 1e15, 1% = 1e16, 10% = 1e17
+    }
+
+    // get platform config
+    function getPlatformConfig() external view returns (PlatformConfig memory);
+
+    // set platform config
+    function setPlatformConfig(PlatformConfig memory config) external;
+
+    // withdraw all platform fee
+    function withdrawPlatformFee() external;
+
+
+    // -------------------------------------------------------------------
+    // Merchant functions
+    // -------------------------------------------------------------------
+    struct Machine {
+
+    }
+
+    // Create a new Channel, any user can create a channel
+    // @return channelID
+    function createChannel() external returns (uint256);
+
+    // Delete a channel, only the channel owner can delete it
+    // All machines in the channel will be deleted
+    function deleteChannel(uint256 _channelId) external;
+
+    // Create a new Machine, only the channel owner can create a machine
+    function createMachine(uint256 _channelId, Machine config) external;
+
+    // Delete a Machine, only the channel owner can delete it
+    function deleteMachine(uint256 _channelId, uint256 _machineId) external;
+
+    // Get Machine Info
+    // @return machine info
+    function getMachineInfo(uint256 _channelId, uint256 _machineId) external view returns (Machine memory);
+
+    // Withdraw all channel fee
+    function withdrawMachineFee(uint256 _channelId) external;
+
+
+    // -------------------------------------------------------------------
+    // Player functions
+    // -------------------------------------------------------------------
+
+    // Roll, will makeRequestUint256()
+    function roll(uint256 _channelId, uint256 _machineId) external;
 }
