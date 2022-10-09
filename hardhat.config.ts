fb3a0f99db7800effc3a8617007e258633f09d7a
tunogya <tunogya@qq.com>
2022-10-09 22:51:54 +0800

    init

---
+++ hardhat.config.ts
@@ -0,0 +1,69 @@
+import * as dotenv from "dotenv";
+
+import { HardhatUserConfig, task } from "hardhat/config";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-waffle";
+import "@openzeppelin/hardhat-upgrades";
+import "@typechain/hardhat";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+import * as process from "process";
+
+dotenv.config();
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+const mnemonic = process.env.HDWALLET_MNEMONIC;
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.9",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      accounts: {
+        mnemonic,
+      },
+    },
+    goerli: {
+      url: process.env.GOERLI_URL || "",
+      accounts: {
+        mnemonic,
+      },
+    },
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    gasPrice: 20,
+    currency: "USD",
+  },
+  etherscan: {
+    apiKey: {
+      goerli: process.env.ETHERSCAN_API_KEY || "",
+    },
+  },
+  mocha: {
+    timeout: 4000,
+    parallel: true,
+  },
+};
+
+export default config;
