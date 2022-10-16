9c3d2bc1105c2e17c0406e228471b82af5f7462b
tunogya <tunogya@qq.com>
2022-10-16 13:29:43 +0800

    update deploy

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,25 +1,31 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
 import { ethers } from "hardhat";
 
+const AirnodeRrpV0: Record<number, string> = {
+  5: "0xa0AD79D995DdeeB18a14eAef56A549A04e3Aa1Bd",
+  1: "0xa0AD79D995DdeeB18a14eAef56A549A04e3Aa1Bd",
+  42161: "0xb015ACeEdD478fc497A798Ab45fcED8BdEd08924",
+  43114: "0xC02Ea0f403d5f3D45a4F1d0d817e7A2601346c9E",
+  56: "0xa0AD79D995DdeeB18a14eAef56A549A04e3Aa1Bd",
+  250: "0xa0AD79D995DdeeB18a14eAef56A549A04e3Aa1Bd",
+  100: "0xa0AD79D995DdeeB18a14eAef56A549A04e3Aa1Bd",
+  1088: "0xC02Ea0f403d5f3D45a4F1d0d817e7A2601346c9E",
+  2001: "0xa0AD79D995DdeeB18a14eAef56A549A04e3Aa1Bd",
+  1284: "0xa0AD79D995DdeeB18a14eAef56A549A04e3Aa1Bd",
+  1285: "0xa0AD79D995DdeeB18a14eAef56A549A04e3Aa1Bd",
+  10: "0xa0AD79D995DdeeB18a14eAef56A549A04e3Aa1Bd",
+  137: "0xa0AD79D995DdeeB18a14eAef56A549A04e3Aa1Bd",
+  30: "0xa0AD79D995DdeeB18a14eAef56A549A04e3Aa1Bd",
+};
+
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
-
-  // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
-
-  await greeter.deployed();
-
-  console.log("Greeter deployed to:", greeter.address);
+  const chainId = (await ethers.provider.getNetwork()).chainId;
+  console.log("Chain ID:", chainId);
+  const airnodeRrp = AirnodeRrpV0[chainId];
+  console.log("AirnodeRrpV0:", airnodeRrp);
+  const Snatch = await ethers.getContractFactory("Snatch");
+  const snatcher = await Snatch.deploy(airnodeRrp);
+  await snatcher.deployed();
+  console.log("Snatch deployed to:", snatcher.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
