1638c1d96a7ef380788bd9c77a5b0d4ee40abf2c
tunogya <tunogya@qq.com>
2023-01-30 22:24:42 +0800

    update

--- scripts/Baccarat/upgrade.js
+++ scripts/Baccarat/upgrade.js
@@ -1,4 +1,4 @@
-import { ethers, upgrades } from "hardhat";
+const { ethers, upgrades } = require("hardhat");
 
 async function main() {
   const chainId = (await ethers.provider.getNetwork()).chainId;
